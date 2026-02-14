2c965b9526c4da4ff53af21128f3c800a3392bb2
chrislamberthome-wq <chrislamberthome@gmail.com>
2026-02-14 03:12:04 -0600

    Create __init__.py in the platoputer_governance/verifiers directory

---
+++ platoputer_governance/verifiers/__init__.py
@@ -0,0 +1,7 @@
+"""
+Platoputer Governance verifiers package.
+
+Intentionally minimal: importing this package must not perform I/O,
+environment reads, or any side effects. Verifiers should be invoked
+explicitly by callers/tests.
+"""
